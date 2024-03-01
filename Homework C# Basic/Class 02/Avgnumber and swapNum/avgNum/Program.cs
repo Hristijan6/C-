6731b28a33cbc129e1944403b67c2ad3c62b8413
Hristijan6 <149889976+Hristijan6@users.noreply.github.com>
2024-03-01 19:12:39 +0100

    Add files via upload

---
+++ Homework C# Basic/Class 02/Avgnumber and swapNum/avgNum/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AverageNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            int number2 = 15;
+            int number3 = 20;
+            int number4 = 30;
+
+
+            int avgNumber = (number1 + number2 + number3 + number4) / 4;
+            Console.WriteLine($"The average {number1}, {number2}, {number3} and {number4} is: {avgNumber}");
+        }
+    }
+}
---
+++ Homework C# Basic/Class 02/Avgnumber and swapNum/swapNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SwapNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int swap;
+
+            swap = number1;
+            number1 = number2;
+            number2 = swap;
+
+            Console.WriteLine($"The first number is {number1} and the second number is {number2}");
+        }
+    }
+}
