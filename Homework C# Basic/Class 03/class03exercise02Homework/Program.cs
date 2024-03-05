b7e4dbdb3225d2456b0842afcac726f53fd84f02
Hristijan6 <149889976+Hristijan6@users.noreply.github.com>
2024-03-06 00:08:17 +0100

    Add files via upload

---
+++ Homework C# Basic/Class 03/class03exercise02Homework/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace class03exercise02Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Numbers from 1 to " + num1 + ":");
+            for (int i = 1; i <= num1; i++)
+            {
+                Console.WriteLine(i + " ");
+            }
+
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Numbers from " + num2 + " to 1:");
+            for (int i = num2; i >= 1; i--)
+            {
+                Console.WriteLine(i + " ");
+            }
+        }
+    }
+}
---
+++ Homework C# Basic/Class 03/class3Homework/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace class3Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1; 
+            int num2;
+            string operation;
+            bool isValidOperation = false;
+
+            Console.Write("Enter the First number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the Second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            while (!isValidOperation)
+            {
+                Console.Write("Enter the Operation (+, -, *, /): ");
+                operation = Console.ReadLine();
+
+                switch (operation)
+                {
+                    case "+":
+                        Console.WriteLine($"The result is: {num1 + num2}");
+                        isValidOperation = true;
+                        break;
+                    case "-":
+                        Console.WriteLine($"The result is: {num1 - num2}");
+                        isValidOperation = true;
+                        break;
+                    case "*":
+                        Console.WriteLine($"The result is: {num1 * num2}");
+                        isValidOperation = true;
+                        break;
+                    case "/":
+                        if (num2 != 0)
+                        {
+                            Console.WriteLine($"The result is: {num1 / num2}");
+                            isValidOperation = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Division by zero!!");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation. Please enter a valid operation.");
+                        break;
+                }
+            }
+        }
+    }
+}
