b953e830c9550a4942d23352252c8e0071004be1
Hristijan6 <149889976+Hristijan6@users.noreply.github.com>
2024-03-10 21:36:08 +0100

    Add files via upload

---
+++ Homework C# Basic/Class 05/Exercise01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your birthday (dd.MM.yyyy):");
+            if (DateTime.TryParse(Console.ReadLine(), out DateTime birthday))
+            {
+                int age = AgeCalculator(birthday);
+                Console.WriteLine($"You are {age} years old.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid date format.");
+            }
+        }
+
+        static int AgeCalculator(DateTime birthday)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - birthday.Year;
+            if (birthday.Date > today.AddYears(-age))
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+}
---
+++ Homework C# Basic/Class 05/Exercise02/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercise02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            if (int.TryParse(Console.ReadLine(), out int n))
+            {
+                Substrings(n);
+            }
+            else
+            {
+                Console.WriteLine("Invalid number.");
+            }
+        }
+
+        static void Substrings(int n)
+        {
+            string glaven = "Hello from SEDC Codeacademy 2024";
+            if (n >= 0 && n <= glaven.Length)
+            {
+                string pomosen = glaven.Substring(0, n);
+                Console.WriteLine($"First {n} characters: {pomosen}");
+                Console.WriteLine($"Length of the new string: {pomosen.Length}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid number. Please enter a number within the range of the string length.");
+            }
+        }        
+    }
+}
