ad4067a34f5ac46a8d80149a11eacd7b8cacc83f
Hristijan6 <149889976+Hristijan6@users.noreply.github.com>
2024-03-11 22:51:19 +0100

    Add files via upload

---
+++ Homework C# Basic/Class 06/Dog.cs
@@ -0,0 +1,34 @@
+﻿public class Dog
+{
+
+	public string DogName { get; set; }
+
+	public string DogBreed { get; set; }
+
+	public string DogColor { get; set; }
+	public Dog()
+	{
+	}
+
+	public Dog(string name, string breed, string color)
+	{
+		DogName = name;
+		DogBreed = breed;
+		DogColor = color;
+	}
+
+	public string Eat()
+	{
+		return $"The dog {DogName} of breed: {DogBreed} and of color: {DogColor} is now eating";
+	}
+
+	public string Play()
+	{
+		return $"The dog {DogName} of breed: {DogBreed} and of color: {DogColor} is now playing";
+	}
+	public string ChaseTail()
+	{
+		return $"The dog {DogName} of breed: {DogBreed} and of color: {DogColor} is now chasing it's tail";
+	}
+}
+
---
+++ Homework C# Basic/Class 06/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Class06Exercise01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the dog's name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter the dog's breed:");
+            string breed = Console.ReadLine();
+
+            Console.WriteLine("Enter the dog's color:");
+            string color = Console.ReadLine();
+
+            Dog dog = new Dog { DogName = name, DogBreed = breed, DogColor = color };
+
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Eat");
+            Console.WriteLine("2. Play");
+            Console.WriteLine("3. Chase Tail");
+
+            int choice;
+            if (int.TryParse(Console.ReadLine(), out choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine(dog.Eat());
+                        break;
+                    case 2:
+                        Console.WriteLine(dog.Play());
+                        break;
+                    case 3:
+                        Console.WriteLine(dog.ChaseTail());
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
+    }
+}
